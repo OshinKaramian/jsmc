973e87ecf472cabcf4afeb977db5b4effea153b0
Oshin Karamian <okaramia@gmail.com>
2016-01-31 23:38:01 -0500

    "Add ffmpeg and nedb"

--- server/src/db.js
+++ server/src/db.js
@@ -1,43 +1,38 @@
 "use strict";
 const Promise = require('bluebird');
-const MongoClient = require('mongodb').MongoClient;
+const path = require('path');
 const config = require('../config/config.json');
-let db;
+const Database = require('nedb');
 
-MongoClient.connect(config.mongoUrl, { promiseLibrary: Promise })
-  .then(function(database) {
-     console.log("Connected correctly to server");
-     db = database;
-  });
+let db = new Database({ filename: path.join(__dirname,'..', 'db', 'jsmc.db'), autoload: true});
+db = Promise.promisifyAll(db);
 
 module.exports.insertQuery = function(collectionName, data) {
-  let db;
-
-  return db.collection('movies').findOne({ "title": data.title  })
+    console.log(data)
+  return db.findOneAsync({ "title": data.title  })
     .then(function(item) {
+        console.log('dog');
       if (!item) {
         console.log('Inserted new item for: ' + data.title);
         data.collection = collectionName;
         console.log(data);
 
-        return db.collection('movies').insertOne(data);
+        return db.insertAsync(data);
       } else {
         console.log('Appending new file for: ' + data.title + ' filename: ' + data.filedata[0].filename);
         item.filedata.push(data.filedata[0]);
-        return db.collection('movies').replaceOne({ "title" : data.title }, item);
+        return db.insertAsync({ "title" : data.title }, item);
       }
     })
     .catch(function(err) {
       console.error(err);
-    }).finally(function() {
-      db.close();
-    });
+    })
 };
 
 module.exports.getMedia = function(collectionName, mediaId) {
-  return db.collection(collectionName).findOne({ 'id' : parseInt(mediaId)});
+  return db.findOneAsync({ 'id' : parseInt(mediaId)});
 };
 
 module.exports.getCollection = function(collectionCategory, collectionName) {
-  return db.collection(collectionCategory).find({ collection: collectionName }).sort({ title: 1}).toArray();
+  return db.findAsync({ collection: collectionName }).sort({ title: 1}).toArray();
 };
