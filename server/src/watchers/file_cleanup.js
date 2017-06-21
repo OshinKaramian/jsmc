87c46845b727e63ef70a722c85c7196a0ce8e4f2
Oshin Karamian <okaramian@brightcove.com>
2017-06-21 16:55:46 -0400

    Add directory manager to help clean up and handle folders

--- server/src/watchers/file_cleanup.js
+++ server/src/watchers/file_cleanup.js
@@ -1,34 +1,20 @@
-'use strict';
-const Promise = require('bluebird');
-const path = require('path');
-const fs = Promise.promisifyAll(require('fs-extra'));
+const DirectoryManager = require('../directory_manager.js');
 
-const config = require('../../config/');
-//const MAX_SIZE = 25000; // 25 Gig Max
-
-const getDirectorySize = (directory) => {
-  return fs.readdirAsync(directory)
-    .then((files) => {
-      const statPromises = files.map((file) => {
-        const fileName = path.join(directory, file);
-        return fs.statAsync(fileName)
-          .then((stat) => {
-            stat.filename = fileName;
-            return stat;
-          });
-      });
-
-      return Promise.all(statPromises);
-    })
-    .then((values) => {
-      const totalFileSize = values.reduce((sum, fileStat) => {
-        console.log(fileStat);
-        return sum + parseInt(fileStat.size);
-      }, 0);
-
-      console.log(totalFileSize / 1000000);
-    })
-    .catch(exception => console.log(exception));
+const convertToMeg = (filesize) => {
+  return filesize / 1000000.0;
 };
 
-getDirectorySize(config.serverOptions.TEMP_DIR);
+const run = async() => {
+  //const mgr = new DirectoryManager(config.serverOptions.TEMP_DIR);
+  const mgr = new DirectoryManager('../server/src/');
+  await mgr.init();
+
+  mgr.stats.forEach(file => console.log(file))
+
+  console.log(mgr.totalSize());
+  console.log(mgr.stats.length);
+  await mgr.deleteToSize(25000);
+  console.log(mgr.stats.length);
+};
+
+run();
