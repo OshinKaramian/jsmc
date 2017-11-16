bd7f4339b59c0d3836e35fe48710d669074f0a4a
Oshin Karamian <okaramia@gmail.com>
2017-11-16 02:28:45 +0000

    Remove linux translation

--- server/src/file.js
+++ server/src/file.js
@@ -64,7 +64,6 @@ if (process.env.JSMC_DEV) {
 
 const ffmpegExe = os.platform() === 'win32' ? 'ffmpeg.exe' : 'ffmpeg';
 const ffprobeExe = os.platform() === 'win32' ? 'ffprobe.exe' : 'ffprobe';
-const platform = os.platform() === 'linux' ? 'darwin' : os.platform();
 ffmpeg.setFfmpegPath(path.join('ffmpeg', platform, 'bin', ffmpegExe));
 ffmpeg.setFfprobePath(path.join('ffmpeg', platform, 'bin', ffprobeExe));
 
